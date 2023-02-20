4033ba77e29b6d1b6c29f7f13f191858d58cfea5
Bernardo <bevgim@gmail.com>
2023-02-20 03:14:24 -0300

    updating csv

--- TP I/im_lazy.py
+++ TP I/im_lazy.py
@@ -1,10 +1,11 @@
-import csv
-import requests
-import json
+from csv import DictReader
+from requests import get
+from json import loads
 
+START = 946
 URL = 'https://pokeapi.glitch.me/v1/pokemon/'
-GEN_RELEASE_DATES = ['February 27, 1996', 
-                     'November 21, 1999', 
+GEN_RELEASE_DATES = ['February 27, 1996',
+                     'November 21, 1999',
                      'November 21, 2002',
                      'September 28, 2006',
                      'September 18, 2010',
@@ -16,8 +17,9 @@ GEN_RELEASE_DATES = ['February 27, 1996',
 
 
 def make_req(pokemon):
-    req = requests.get(URL + pokemon).text
-    gen = json.loads(req)
+    req = get(URL + pokemon).text
+    print(req)
+    gen = loads(req)
 
     if isinstance(gen, list):
         gen = gen[0]
@@ -26,21 +28,21 @@ def make_req(pokemon):
 
 
 if __name__ == '__main__':
-    with open('csv/edited_pokedex.csv', 'w') as new_file:
+    with open('csv/yet_another_edited_pokedex.csv', 'w') as new_file:
         file = open('csv/pokedex.csv')
         csv_file = file.readlines()
         file.seek(0)
-        csv_reader = csv.DictReader(file)
+        csv_reader = DictReader(file)
         i = 0
 
-        headers = csv_file[i]
-        new_file.write(headers[:len(headers) - 1] + ',gen,release_date\n')
+        # headers = csv_file[i]
+        # new_file.write(headers[:len(headers) - 1] + ',gen,release_date\n')
 
         for row in csv_reader:
             i += 1
             pokemon = row.get('name')
 
-            if pokemon == None:
+            if pokemon == None or i < START:
                 continue
 
             gen = make_req(pokemon)
