fe529233341be3c564210541fae7e60cb813b40b
ToniLommez <tonilommez@hotmail.com>
2023-02-20 00:10:34 -0300

    pokedex

--- TP I/main.go
+++ TP I/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello World")
+	// Abrir o arquivo CSV
+	file, err := os.Open("csv/pokedex.csv")
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo:", err)
+		return
+	}
+	defer file.Close()
+
+	// Criar um leitor CSV
+	reader := csv.NewReader(file)
+
+	// Ler as linhas do arquivo
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+		return
+	}
+
+	// Imprimir o conteúdo do arquivo
+	for _, record := range records {
+		fmt.Println(record)
+	}
 }
