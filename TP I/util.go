ea916d4cfa38bfdeded1b12df4cc8c3e155e3fe6
Bernardo <bevgim@gmail.com>
2023-02-23 20:47:47 -0300

    joined two files

--- TP I/util.go
+++ TP I/util.go
@@ -8,11 +8,28 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+    "encoding/binary"
+    "math"
 )
 
 func Atoi32(s string) (int32, error) {
 	i, err := strconv.Atoi(s)
 	return int32(i), err
+}
+
+// Funcao para converter int32 para []byte
+func IntToBytes(n int32) []byte {
+	var buf []byte
+	return binary.LittleEndian.AppendUint32(buf, uint32(n))
+}
+
+// Funcao para converter float32 para []byte
+func FloatToBytes(f float32) []byte {
+	b := make([]byte, 4)
+	bits := math.Float32bits(f)
+	binary.LittleEndian.PutUint32(b, bits)
+
+	return b
 }
 
 func RemoveAfterSpace(str string) string {
