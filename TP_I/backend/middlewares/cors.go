640f54c1b03d79e572d063af42ee0d4e087c9fde
ToniLommez <tonilommez@hotmail.com>
2023-03-04 05:52:10 -0300

    handlers.go e cors

--- TP_I/backend/middlewares/cors.go
+++ TP_I/backend/middlewares/cors.go
@@ -6,8 +6,6 @@ func EnableCORS(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// adiciona o header de Access-Control-Allow-Origin para permitir todos os origens
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
