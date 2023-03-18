848c37d64d9eceefdd08328339456f4195fc1f5e
Bernardo <bevgim@gmail.com>
2023-03-18 13:11:03 -0300

    we are back with python

---
+++ Backend/dataManager/add_data.py
@@ -0,0 +1,56 @@
+import csv
+import urllib.request
+
+IN_FILE = '../data/pokedex.csv'
+OUT_FILE = '../data/pokedex2.csv'
+URL = 'https://raw.githubusercontent.com/PokeAPI/pokeapi/master/data/v2/csv/pokemon_species_flavor_text.csv'
+
+def get_descriptions() -> str:
+    try:
+        with urllib.request.urlopen(URL) as f:
+            return f.read().decode('utf-8')
+    except urllib.error.URLError as e:
+        return e.reason
+    
+
+def parse_description(string: str, id: int, start: int) -> tuple[int, str]:
+    token = f'{id},18,9'
+    index = string.find(token, start) + len(token) + 2
+    description = ''
+
+    while string[index] != '"':
+        if string[index] != '\n':
+            description += string[index]
+        else:
+            description += ' '
+        index += 1
+
+    return index, description
+
+
+if __name__ == '__main__':
+    with open(OUT_FILE, 'w') as new_file:
+        file = open(IN_FILE)
+        csv_file = file.readlines()
+        file.seek(0)
+        csv_reader = csv.DictReader(file)
+        descriptions = get_descriptions()
+        last_index = 0
+        i = 0
+
+        headers = csv_file[i]
+        new_file.write(headers[:len(headers) - 1] + ',description\n')
+
+        for row in csv_reader:
+            i += 1
+            poke_id = row.get('pokedex_number')
+
+            if poke_id == None:
+                continue
+
+            last_index, description = parse_description(descriptions, poke_id, last_index)
+
+            line = csv_file[i]
+            new_file.write(line[:len(line) - 1] + f'"{description}"\n')
+
+        file.close()
