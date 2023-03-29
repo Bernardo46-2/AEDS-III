c348d2a8f4a9eee690454cda484d784191848cb6
Bernardo <bevgim@gmail.com>
2023-03-29 11:42:48 -0300

    better menu

--- Backend/main.go
+++ Backend/main.go
@@ -9,15 +9,9 @@
 package main
 
 import (
-	// "bufio"
-	"bufio"
 	"fmt"
 	"net/http"
 	"os"
-	"strconv"
-
-	// "os"
-	// "strconv"
 
 	"github.com/Bernardo46-2/AEDS-III/dataManager"
 	"github.com/Bernardo46-2/AEDS-III/handlers"
@@ -26,24 +20,16 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("1 - Hashing Dinamico")
-	fmt.Println("2 - Importar CSV")
-	fmt.Println("3 - B Tree")
-	fmt.Println("9 - Ligar Servidor")
-	scanner.Scan()
-	opcao, _ := strconv.Atoi(scanner.Text())
-	fmt.Println()
-	switch opcao {
-	case 1:
-		dataManager.StartHashFile()
-	case 2:
-		dataManager.ImportCSV().CsvToBin()
-	case 3:
-		dataManager.StartBTreeFile()
-	case 9:
+	switch os.Args[1] {
+	case "1", "server":
 		fmt.Println("Servidor Iniciado")
 		servidor()
+	case "2", "csv":
+		dataManager.ImportCSV().CsvToBin()
+	case "3", "hash":
+		dataManager.StartHashFile()
+	case "4", "btree":
+		dataManager.StartBTreeFile()
 	default:
 		fmt.Println("Opção inválida")
 	}
