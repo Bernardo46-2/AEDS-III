31c7d7dcc231463e05dde7d54ca2117100d6e3c9
ToniLommez <tonilommez@hotmail.com>
2023-05-19 15:11:26 -0300

    correcao inicial do huffman

--- Backend/data/compress/huffman/huffman.go
+++ Backend/data/compress/huffman/huffman.go
@@ -10,4 +10,19 @@ func Zip(path string) error {
 	if err != nil {
 		return fmt.Errorf("erro do tipo: %s", err.Error())
 	}
+	defer file.Close()
+
+	content := make([]byte, 0)
+	buffer := make([]byte, 1024)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		content = append(content, buffer[:n]...)
+	}
+
+	fmt.Println(string(content))
+
+	return nil
 }
